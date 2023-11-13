fc3d763fe821b4ddcc1a08767b063dbd847a9af1
zwj <1927773824@qq.com>
2023-11-13 19:25:18 +0800

    解决reqire

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import fs from "fs"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'gaussian-splatting-webgl',
+  base: '/gaussian-splatting-webgl/',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
@@ -16,4 +16,9 @@ export default defineConfig({
       cert: fs.readFileSync("./ssl/119.crt")
     },
   },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 })
